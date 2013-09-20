159be8b91c72dc6465744e20daa8a39b6c712c7e
Alex Jeffery <apj21@sussex.ac.uk>
2013-09-20 17:06:22 +0000

    Continued AST

--- Syntax.hs
+++ Syntax.hs
@@ -1,26 +1,28 @@
 -- This module defines the abstract syntax of the language
-
 module Syntax where
 
-data Program = String Expression Program
+data Program = Declaration String Expression
              | EndProgram
-             deriving Show
 
-data Expression = Int
-                | Expression Op Expression
-                deriving Show
+type Name = String
 
-data Op = Add
-        | Sub
-        | Mul
-        | Div
-        | Mod
-        deriving Show
+-- Lambda Expressions need a mechanism to handle naming clashes when replacing
+-- variables (bound variables with mathcing names should be renamed
+data Expression = Lambda Name Expression
+                | Name
+                | Application Expression Expression
+-- Non-Lambda Expressions:
+                | Literal Int
+                | Arithmetic Expression Op Expression
+                | IfThenElse Expression Expression Expression
+
+data Op = Add | Sub | Mul | Div | Mod
 
 eval :: Expression -> Int
-eval (a Add b) = (eval a) + (eval b)
-eval (a Sub b) = (eval a) - (eval b)
-eval (a Mul b) = (eval a) * (eval b)
-eval (a Div b) = div (eval a) (eval b)
-eval (a Mod b) = mod (eval a) (eval b)
-eval       val = val
+eval (Arithmetic x Add y) = (eval x) + (eval y)
+eval (Arithmetic x Sub y) = (eval x) - (eval y)
+eval (Arithmetic x Mul y) = (eval x) * (eval y)
+eval (Arithmetic x Div y) = div (eval x) (eval y)
+eval (Arithmetic x Mod y) = mod (eval x) (eval y)
+eval (IfThenElse i t e)   = if (eval i) /= 0 then (eval t) else (eval e)
+eval (Literal x)          = x
