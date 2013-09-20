50a3ff3a4d18fc96aba37fc18ab10e04f6bdfc91
Alex Jeffery <apj21@sussex.ac.uk>
2013-09-20 09:36:11 +0100

    Continued AST

--- Syntax.hs
+++ Syntax.hs
@@ -4,23 +4,23 @@ module Syntax where
 
 data Program = String Expression Program
              | EndProgram
-  deriving Show
+             deriving Show
 
 data Expression = Int
                 | Expression Op Expression
-  deriving Show
+                deriving Show
 
 data Op = Add
         | Sub
         | Mul
         | Div
         | Mod
-  deriving Show
+        deriving Show
 
 eval :: Expression -> Int
-eval a Add b = (eval a) + (eval b)
-eval a Sub b = (eval a) - (eval b)
-eval a Mul b = (eval a) * (eval b)
-eval a Div b = div (eval a) (eval b)
-eval a Mod b = mod (eval a) (eval b)
-eval val = val
+eval (a Add b) = (eval a) + (eval b)
+eval (a Sub b) = (eval a) - (eval b)
+eval (a Mul b) = (eval a) * (eval b)
+eval (a Div b) = div (eval a) (eval b)
+eval (a Mod b) = mod (eval a) (eval b)
+eval       val = val
