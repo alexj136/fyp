35dd1ecfe2e556f55d668cb7660a53a4aa0e4b80
Alex Jeffery <apj21@sussex.ac.uk>
2013-09-30 15:51:40 +0000

    Continued name clash handling

--- Syntax.hs
+++ Syntax.hs
@@ -39,16 +39,29 @@ rename ns (Lambda n x) | any (== n) ns =
 rename ns (Name n)     | any (== n) ns = Name (n ++ "'")
                        |
 
--- Rename ONLY BOUND instances of the given name in the given expression
--- recurse down the tree until we find the given binding, at which point
--- we can do a careless rename of every instance of that name that we
--- find
-renameBound ::
+-- FINAL REPLACEMENT ALGORITHM
+-- Create a list of unbound names in the argument expression
+-- For each name in that list
+--     call newName to get a name that doesn't exist in function body
+--     call renameBound newName functionBody to do the replacement
+-- Finally carry out the substitution
 
+-- Rename ONLY BOUND instances of the given name in the given expression
+-- recurse down the tree until we find the given binding, at which point we can
+-- do a careless rename of every instance of that name that we find. If we find
+-- the name before we find the binding, we don't need to do anything - the name
+-- is not bound at that point and therefore doesn't need renaming.
+renameBound :: String -> String -> Expression -> Expression
+renameBound from to (Lambda n x)          | n == from = Lambda to (blindRename from to x)
+                                          | otherwise = Lambda n (renameBound from to x)
+renameBound from to (Application x x')    = Application (renameBound from to x) (renameBound from to x')
+renameBound from to (Arithemtic x op x')  = Arithmetic (renameBound from to x) op (renameBound from to x')
+renameBound from to (IfThenElse x x' x'') = IfThenElse (rB x) (rB x') (rB x'') where rB = renameBound from to
+renameBound _    _  x                     = x
 
 -- Returns a string which does not exist as a name within a given Expression
 newName :: Expression -> String
-newname _ = error "Not yet implemented"
+newName _ = error "newName not yet implemented"
 
 -- Renames every name with value 'from' to value 'to', with no regard for
 -- clashes etc
@@ -58,20 +71,29 @@ blindRename from to (Lambda n x)          | n == from = Lambda to (blindRename n
 blindRename from to (Application x x')    = Application (blindRename from to x) (blindRename from to x')
 blindRename from to (Name n)              = if n == from then (Name to) else (Name n)
 blindRename from to (Arithmetic x op x')  = Arithmetic (blindRename from to x) op (blindRename from to x')
-blindRename from to (IfThenElse x x' x'') = IfThenElse (blindRename from to x) (blindRename from to x') (blindRename from to x'')
+blindRename from to (IfThenElse x x' x'') = IfThenElse (bR x) (bR x') (bR x'') where bR = blindRename from to
 blindRename _    _  (Literal x)           = Literal x
+
+-- Returns true if there are ANY occurences of the given string as a name in
+-- the given Expression
+nameIn :: String -> Expression -> Bool
+nameIn n (Lambda n' x)         = (n == n') || nameIn n x
+nameIn n (Name n')             = n == n'
+nameIn n (Application x x')    = any (nameIn n) [x, x']
+nameIn n (Arithmetic x _ x')   = any (nameIn n) [x, x']
+nameIn n (IfThenElse x x' x'') = any (nameIn n) [x, x', x'']
+nameIn _ (Literal _)           = False
 
 -- Returns true unless there are free occurences of the given name in the given
 -- Expression
-boundIn :: Expression -> Expression -> Bool
-boundIn (Name n) (Lambda n' x)         | n == n'   = True
-                                       | otherwise = boundIn (Name n) x
-boundIn (Name n) (Application x x')    = all (boundIn (Name n)) [x, x']
-boundIn (Name n) (Name n')             = n /= n'
-boundIn (Name n) (Arithmetic x _ x')   = all (boundIn (Name n)) [x, x']
-boundIn (Name n) (IfThenElse x x' x'') = all (boundIn (Name n)) [x, x', x'']
-boundIn (Name n) (Literal _)           = True
-boundIn _ _ = error "Invalid arguments to boundIn (first arg must be a Name)"
+boundIn :: String -> Expression -> Bool
+boundIn n (Lambda n' x)         | n == n'   = True
+                                | otherwise = boundIn (Name n) x
+boundIn n (Application x x')    = all (boundIn (Name n)) [x, x']
+boundIn n (Name n')             = n /= n'
+boundIn n (Arithmetic x _ x')   = all (boundIn (Name n)) [x, x']
+boundIn n (IfThenElse x x' x'') = all (boundIn (Name n)) [x, x', x'']
+boundIn n (Literal _)           = True
 
 -- Creates a set of all unbound names in an Expression
 getNames :: Set.Set String -> Set.Set String -> Expression -> Set.Set String
